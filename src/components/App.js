c75f0ef41edd8da9371a66a45fe906a38e556d3c
heejung <dap072055@naver.com>
2022-03-28 15:09:33 +0900

    last programming

--- src/components/App.js
+++ src/components/App.js
@@ -1,66 +1,102 @@
 import { useEffect, useState } from "react";
-import { getReviews } from "../api";
 import ReviewList from "./ReviewList";
+import ReviewForm from "./ReviewForm";
+import { createReview, deleteReview, getReviews, updateReview } from "../api";
+import LocaleContext from "../contexts/LocaleContext";
+
 const LIMIT = 6;
+
 function App() {
-  const [items, setItems] = useState([]);
   const [order, setOrder] = useState("createdAt");
   const [offset, setOffset] = useState(0);
   const [hasNext, setHasNext] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [loadingError, setLoadingError] = useState(null);
+  const [items, setItems] = useState([]);
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
+
   const handleNewestClick = () => setOrder("createdAt");
 
   const handleBestClick = () => setOrder("rating");
 
-  const handleDelete = (id) => {
-    const nextItems = items.filter((item) => item.id !== id);
-    setItems(nextItems);
+  const handleDelete = async (id) => {
+    const result = await deleteReview(id);
+    if (!result) return;
+
+    setItems((prevItems) => prevItems.filter((item) => item.id !== id));
   };
 
   const handleLoad = async (options) => {
     let result;
     try {
-      setIsLoading(true);
       setLoadingError(null);
-      result = await getReviews(options); //fullfilled 상태가 될때까지 기다린다.
+      setIsLoading(true);
+      result = await getReviews(options);
     } catch (error) {
       setLoadingError(error);
       return;
     } finally {
       setIsLoading(false);
     }
+
     const { paging, reviews } = result;
     if (options.offset === 0) {
       setItems(reviews);
     } else {
-      setItems([...items, ...reviews]);
+      setItems((prevItems) => [...prevItems, ...reviews]);
     }
     setOffset(options.offset + options.limit);
     setHasNext(paging.hasNext);
   };
 
   const handleLoadMore = async () => {
-    handleLoad({ order, offset, limit: LIMIT });
+    await handleLoad({ order, offset, limit: LIMIT });
   };
+
+  const handleCreateSuccess = (review) => {
+    setItems((prevItems) => [review, ...prevItems]);
+  };
+
+  const handleUpdateSuccess = (review) => {
+    setItems((prevItems) => {
+      const splitIdx = prevItems.findIndex((item) => item.id === review.id);
+      return [
+        ...prevItems.slice(0, splitIdx),
+        review,
+        ...prevItems.slice(splitIdx + 1),
+      ];
+    });
+  };
+
   useEffect(() => {
     handleLoad({ order, offset: 0, limit: LIMIT });
   }, [order]);
+
   return (
-    <div>
+    <LocaleContext.Provider value={"ko"}>
       <div>
-        <button onClick={handleNewestClick}>최신순</button>
-        <button onClick={handleBestClick}>베스트순</button>
+        <div>
+          <button onClick={handleNewestClick}>최신순</button>
+          <button onClick={handleBestClick}>베스트순</button>
+        </div>
+        <ReviewForm
+          onSubmit={createReview}
+          onSubmitSuccess={handleCreateSuccess}
+        />
+        <ReviewList
+          items={sortedItems}
+          onDelete={handleDelete}
+          onUpdate={updateReview}
+          onUpdateSuccess={handleUpdateSuccess}
+        />
+        {hasNext && (
+          <button disabled={isLoading} onClick={handleLoadMore}>
+            더 보기
+          </button>
+        )}
+        {loadingError?.message && <span>{loadingError.message}</span>}
       </div>
-      <ReviewList items={sortedItems} onDelete={handleDelete} />
-      {hasNext && (
-        <button disabled={isLoading} onClick={handleLoadMore}>
-          더보기
-        </button>
-      )}
-      {loadingError?.message && <span>{loadingError.message}</span>}
-    </div>
+    </LocaleContext.Provider>
   );
 }
 
--- src/components/ReviewList.js
+++ src/components/ReviewList.js
@@ -1,39 +1,84 @@
+import { useContext } from "react";
+import { useState } from "react";
+import LocaleContext from "../contexts/LocaleContext";
+import Rating from "./Rating";
+import ReviewForm from "./ReviewForm";
 import "./ReviewList.css";
 
 function formatDate(value) {
   const date = new Date(value);
   return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}`;
 }
-function ReviewListItem({ item, onDelete }) {
+
+function ReviewListItem({ item, onDelete, onEdit }) {
+  const locale = useContext(LocaleContext);
   const handleDeleteClick = () => {
     onDelete(item.id);
   };
+
+  const handleEditClick = () => {
+    onEdit(item.id);
+  };
+
   return (
     <div className="ReviewListItem">
-      <img className="ReviewList-img" src={item.imgUrl} alt={item.title} />
+      <img className="ReviewListItem-img" src={item.imgUrl} alt={item.title} />
       <div>
         <h1>{item.title}</h1>
-        <p>{item.rating}</p>
+        <Rating value={item.rating} />
         <p>{formatDate(item.createdAt)}</p>
         <p>{item.content}</p>
+        <p>현재 언어: {locale}</p>
+        <button onClick={handleEditClick}>수정</button>
         <button onClick={handleDeleteClick}>삭제</button>
       </div>
     </div>
   );
 }
 
-function ReviewList({ items, onDelete }) {
+function ReviewList({ items, onUpdate, onUpdateSuccess, onDelete }) {
+  const [editingId, setEditingId] = useState(null);
+
+  const handleCancel = () => setEditingId(null);
+
   return (
     <ul>
-      {" "}
       {items.map((item) => {
+        if (item.id === editingId) {
+          const { id, imgUrl, title, rating, content } = item;
+          const initialValues = { title, rating, content, imgFile: null };
+
+          const handleSubmit = (formData) => onUpdate(id, formData);
+
+          const handleSubmitSuccess = (review) => {
+            onUpdateSuccess(review);
+            setEditingId(null);
+          };
+
+          return (
+            <li key={item.id}>
+              <ReviewForm
+                initialValues={initialValues}
+                initialPreview={imgUrl}
+                onSubmit={handleSubmit}
+                onSubmitSuccess={handleSubmitSuccess}
+                onCancel={handleCancel}
+              />
+            </li>
+          );
+        }
         return (
           <li key={item.id}>
-            <ReviewListItem item={item} onDelete={onDelete} />
+            <ReviewListItem
+              item={item}
+              onDelete={onDelete}
+              onEdit={setEditingId}
+            />
           </li>
         );
-      })}{" "}
+      })}
     </ul>
   );
 }
+
 export default ReviewList;
